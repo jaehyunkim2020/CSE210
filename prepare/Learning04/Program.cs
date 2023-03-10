fac8686cb096a266c73c90dd14c814ec639dcdbd
jaehyunkim2020 <jaehyunkim2020@gmail.com>
2023-03-09 17:10:02 -0700

    W07 Prepare inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment ma1 = new MathAssignment("Samuel Bennett", "Multiplication", "Chapter 2", "Exercises 2.1-2.5");
+        System.Console.WriteLine(ma1.GetSummary());
+        System.Console.WriteLine(ma1.GetHomeworkList());
+
+        WritingAssignment wa1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        System.Console.WriteLine(wa1.GetSummary());
+        System.Console.WriteLine(wa1.GetWritingInformation());
     }
 }
